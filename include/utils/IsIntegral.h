6b207b6a80cc877e35a16509370a6a85356460f1
Vinicius Cordeiro <viniciuscordeirop@gmail.com>
2022-07-30 20:34:33 -0300

    Fix implementation

--- include/utils/IsIntegral.h
+++ include/utils/IsIntegral.h
@@ -4,6 +4,8 @@
 #ifndef IS_INTEGRAL_H
 #define IS_INTEGRAL_H
 
+#include "RemoveCV.h"
+
 namespace ft
 {
 
@@ -18,44 +20,59 @@ namespace ft
 *
 */
 
+template< typename T, T v >
+struct integral_constant
+{
+    static const T value = v;
+    typedef T value_type;
+    typedef integral_constant< T, v > type;
+    operator value_type() const { return value; }
+};
+
+typedef integral_constant< bool, true > true_type;
+typedef integral_constant< bool, false > false_type;
+
 /*
 *   @param T     A type to check.
 */
-template < typename T >
-struct is_integral { static const bool value = false; };
+template< typename T >
+struct is_integral : public false_type {};
 
-template <>
-struct is_integral< bool > { static const bool value = true; };
+template<>
+struct is_integral< bool > : public true_type {};
     
-template <>
-struct is_integral< char > { static const bool value = true; };
+template<>
+struct is_integral< char > : public true_type {};
 
-template <>
-struct is_integral< unsigned  char> { static const bool value = true; };
+template<>
+struct is_integral< unsigned char > : public true_type {};
 
-template <>
-struct is_integral< short > { static const bool value = true; };
+template<>
+struct is_integral< signed char > : public true_type {};
 
-template <>
-struct is_integral< unsigned  short> { static const bool value = true; };
+template<>
+struct is_integral< short > : public true_type {};
 
-template <>
-struct is_integral< int > { static const bool value = true; };
+template<>
+struct is_integral< unsigned short > : public true_type {};
 
-template <>
-struct is_integral< unsigned  int> { static const bool value = true; };
+template<>
+struct is_integral< int > : public true_type {};
 
-template <>
-struct is_integral< long > { static const bool value = true; };
+template<>
+struct is_integral< unsigned int > : public true_type {};
 
-template <>
-struct is_integral< unsigned  long> { static const bool value = true; };
+template<>
+struct is_integral< long > : public true_type {};
 
-template <>
-struct is_integral< long  long> { static const bool value = true; };
+template<>
+struct is_integral< unsigned long > : public true_type {};
 
-template <>
-struct is_integral< unsigned  long long> { static const bool value = true; };
+template<>
+struct is_integral< long long > : public true_type {};
+
+template<>
+struct is_integral< unsigned long long > : public true_type {};
 
 } // ft
 
